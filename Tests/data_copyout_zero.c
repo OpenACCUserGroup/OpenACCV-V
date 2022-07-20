dfda8d35e6a4a157f10c97ad63725edd3962dc7c
Aaron Jarmusch <jarmusch@udel.edu>
2022-07-20 16:57:46 -0400

    New tests for the zero modifier

---
+++ Tests/data_copyout_zero.c
@@ -0,0 +1,54 @@
+#include "acc_testsuite.h"
+#ifndef T1
+//T1:data,executable-data,data-region,V:3.0-3.2
+int test1(){
+
+    int err = 0;
+    srand(SEED);
+
+    real_t * a = (real_t *)malloc(n * sizeof(real_t));
+    real_t * b = (real_t *)malloc(n * sizeof(real_t));
+
+    for (int x = 0; x < n; ++x){
+        a[x] = rand() / (real_t)(RAND_MAX / 10);
+        b[x] = 0;
+    }
+
+    #pragma acc data copyin(a[0:n]) copyout(zero: b[0:n])
+    {
+      #pragma acc parallel
+      {
+        #pragma acc loop
+        {
+          for (int x = 0; x < n; ++x){
+            b[x] += a[x];
+          }
+        }
+      }
+    }
+
+    for (int x = 0; x < n; ++x){
+        if(fabs(a[x] - b[x]) > PRECISION){
+                err += 1;
+                break;
+        }
+    }
+
+    return err;
+}
+#endif
+
+int main(){
+    int failcode = 0;
+    int failed;
+#ifndef T1
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test1();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 0);
+    }
+#endif
+    return failcode;
+}
