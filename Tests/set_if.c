c1c713bbf6ad5a4c2b88f6790e434bde7a78697b
Aaron Jarmusch <jarmusch@udel.edu>
2022-07-20 16:45:47 -0400

    New tests for the if clause

---
+++ Tests/set_if.c
@@ -0,0 +1,54 @@
+#include "acc_testsuite.h"
+
+/*
+ * if clause not implimented for set as of now
+*/
+
+#ifndef T1
+//T1:,V:2.7-3.0
+int test1(){
+	int err = 0;
+	srand(SEED);
+	int device_type = acc_get_device_type();
+
+	#pragma acc set if(acc_get_device_type == device_type)
+
+	return err;	
+}
+#endif
+#ifndef T2
+//T2:,V:2.7-3.0
+int test2(){
+	int err = 0;
+        srand(SEED);
+        int device_type = acc_get_device_type();
+
+        #pragma acc set if(acc_get_device_type != device_type)
+
+        return err;
+}
+#endif
+
+int main(){
+    int failcode = 0;
+    int failed;
+#ifndef T1
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test1();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 0);
+    }
+#endif
+#ifndef T2
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test2();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 1);
+    }
+#endif
+    return failcode;
+}
