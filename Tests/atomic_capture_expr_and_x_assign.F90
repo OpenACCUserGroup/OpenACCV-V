7cb389bfd67cb93b3b07a9fc87b9685ad6f9d129
Alister Johnson <alisterj@nvidia.com>
2025-03-14 09:11:21 -0700

    fixing misc typos, including a loop index and a couple #ifndefs

--- Tests/atomic_capture_expr_and_x_assign.F90
+++ Tests/atomic_capture_expr_and_x_assign.F90
@@ -2,7 +2,7 @@
 #define ATOMIC_LOGICAL
 #define ATOMIC_OPTYPE EXPR_AND_X
 #define ATOMIC_ASSIGN_LATER
-#define ATOMIC_INIT .FALSE.
+#define ATOMIC_INIT .TRUE.
 #define ATOMIC_SIZE 10
 #ifndef T1
 !T1:construct-independent,atomic,V:2.0-2.7
