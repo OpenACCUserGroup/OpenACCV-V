7d31deaceaccbb8751ab551bd701152469a71fdc
Jay Patel <jaypat@x1403c7s3b0n0h0.chn.perlmutter.nersc.gov>
2025-03-27 12:23:09 -0700

    updated with new tests

--- Tests/copy_copyout.c
+++ Tests/copy_copyout.c
@@ -1,7 +1,50 @@
 #include "acc_testsuite.h"
 #ifndef T1
-//T1: , V:1.0-2.7
+//T1: , V:1.0-3.3
 int test1(){
+    int err = 0;
+    real_t test = 0;
+    #pragma acc parallel loop copyout(test) copy(test) reduction(+:test)
+    for( int x = 0; x <n; ++x){
+        test += 1;
+    }
+
+    if(fabs(test - n) > PRECISION){
+        err++;
+    }
+
+    return err;
+}
+#endif
+
+#ifndef T2
+//T2: , V:1.0-3.3
+int test2(){
+    int err = 0;
+    real_t *test = (real_t *)malloc(n * sizeof(real_t));
+
+    for(int x = 0; x < n; ++x){
+        test[x] = 1.0;
+    }
+
+   #pragma acc parallel loop copyout(test[0:n]) copy(test[0:n])
+   for(int x = 0; x < n; ++x){
+        test[x] += 1.0;
+   }
+
+   for(int x = 0; x < n; ++x){
+        if(fabs(test[x] - 2.0) > PRECISION){
+            err++;
+        }
+    }
+
+    return err;
+}
+#endif
+
+#ifndef T3
+//T3: , V:1.0-2.7
+int test3(){
     int err = 0;
     real_t test = 0;
     #pragma acc parallel loop copy(test) copyout(test) reduction(+:test)
@@ -17,9 +60,9 @@ int test1(){
 }
 #endif
 
-#ifndef T2
-//T2: , V:1.0-2.7
-int test2(){
+#ifndef T4
+//T4: , V:1.0-2.7
+int test4(){
     int err = 0;
     real_t *test = (real_t *)malloc(n * sizeof(real_t));
 
@@ -27,7 +70,7 @@ int test2(){
         test[x] = 1.0;
     }
 
-   #pragma acc parallel loop copy(test[0:n]) copyout(test[0:n])
+   #pragma acc parallel loop copy(test[0:n]) copyout(test[0:n]) 
    for(int x = 0; x < n; ++x){
         test[x] += 1.0;
    }
@@ -47,20 +90,38 @@ int main(){
     int failed;
 #ifndef T1
     failed = 0;
-    for( int x = 0; x < NUM_TEST_CALLS; ++x){
-	failed += test1();
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test1();
     }
-    if(failed){
-	failcode += (1 << 0);
+    if (failed != 0){
+        failcode = failcode + (1 << 0);
     }
 #endif
 #ifndef T2
     failed = 0;
-    for( int x = 0; x < NUM_TEST_CALLS; ++x){
-        failed += test2();
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test2();
     }
-    if(failed){
-        failcode += (1 << 1);
+    if (failed != 0){
+        failcode = failcode + (1 << 1);
+    }
+#endif
+#ifndef T3
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test3();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 2);
+    }
+#endif
+#ifndef T4
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test4();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 3);
     }
 #endif
     return failcode;
