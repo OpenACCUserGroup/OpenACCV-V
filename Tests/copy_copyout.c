8307d4e09a746a5882a27c4baef1758f9427e9e5
Jay Patel <jaypat@login08.chn.perlmutter.nersc.gov>
2025-03-31 12:39:33 -0700

    updated with new tests

--- Tests/copy_copyout.c
+++ Tests/copy_copyout.c
@@ -1,6 +1,14 @@
 #include "acc_testsuite.h"
 #ifndef T1
 //T1: , V:1.0-3.3
+
+/*
+Performs a scalar summation using a parallel loop with both copyout(test) and copy(test) 
+along with a reduction(+:test) clause. Verifies that the compiler correctly handles 
+combined data clauses during scalar reduction. Test checks to make sure, variable "tests"
+is equal to value of n
+*/
+
 int test1(){
     int err = 0;
     real_t test = 0;
@@ -19,6 +27,14 @@ int test1(){
 
 #ifndef T2
 //T2: , V:1.0-3.3
+
+/*
+Initializes an array of 1.0s, then uses a parallel loop with both copyout(test[0:n]) 
+and copy(test[0:n]) to increment each element. Checks whether the final values are 
+correctly updated to 2.0. Verifies that the compiler correctly handles combined data 
+clauses using arrays
+*/
+
 int test2(){
     int err = 0;
     real_t *test = (real_t *)malloc(n * sizeof(real_t));
@@ -44,6 +60,11 @@ int test2(){
 
 #ifndef T3
 //T3: , V:1.0-2.7
+
+/* 
+Identical to T1 but swaps the order of copy and copyout clauses. 
+*/
+
 int test3(){
     int err = 0;
     real_t test = 0;
@@ -62,6 +83,11 @@ int test3(){
 
 #ifndef T4
 //T4: , V:1.0-2.7
+
+/* 
+Identical to T2 but swaps the order of copy and copyout clauses. 
+*/
+
 int test4(){
     int err = 0;
     real_t *test = (real_t *)malloc(n * sizeof(real_t));
