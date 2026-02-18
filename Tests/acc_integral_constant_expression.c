7aeb8973f00d59509602ed6134f0b70d94880d71
Ryanpadrone <159075564+Ryanpadrone@users.noreply.github.com>
2026-02-18 18:50:54 -0500

    Update acc_integral_constant_expression.c

--- Tests/acc_integral_constant_expression.c
+++ Tests/acc_integral_constant_expression.c
@@ -1,12 +1,29 @@
 // acc_integral_constant_expression.c
+//
 //Feature under test (OpenACC 3.4, Section 1.6, Feb 2026):
 // - Clause arguments that require an integral-constant-expression accept
 //    C integral constant expressions (macros and enum values).
 // 
 //  Notes:
-//  - Uses integral constant expressions in: collapse, tile, cache (index and slice),
-//   and gang(dim:) to validate compile-time constant handling.
-
+//  T1: collapse() accepts an integral-constant-expression (ICE).
+//   This test uses a macro ICE in collapse() and checks runtime correctness.
+//  
+//  T2: tile() accepts an integral-constant-expression (ICE).
+//   This test uses a macro ICE in tile() and checks runtime correctness.
+//
+//  T3: tile() accepts ICE values (enums/macros).
+//   This test mixes enum + macro ICE in tile() and checks correctness.
+//
+//  T4: cache() indexing accepts an integral-constant-expression (ICE).
+//   This test uses an enum ICE as the cache element index.
+//
+//  T5: cache() subarray slices accept ICE values.
+//   This test uses enum/macro ICE for lower:length in cache().
+//
+//  T6: gang(dim:) accepts an integral-constant-expression (ICE) (valid range 1..3).
+//   This test uses an enum ICE in gang(dim:) and checks runtime correctness.
+// – Some compilers may not support the 'dim:' keyword form yet; keep this as spec conformance coverage.
+//
 
 #include "acc_testsuite.h"
 #include <openacc.h>
@@ -21,8 +38,6 @@ enum { ICE_ENUM2 = 2, ICE_ENUM4 = 4, ICE_ENUM_LOWER = 3 };
 #define ICE_LEN4     (4)
 
 #ifndef T1
-//T1:syntax,collapse-clause,runtime,loop,V:3.4-
-// collapse(ICE_MACRO2) macro arithmetic ICE
 int test1(void){
     int err = 0;
     const int M = 48, N = 12;
@@ -36,7 +51,11 @@ int test1(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<MN;i++){ a[i]=(real_t)(i+2); b[i]=(real_t)(i-1); c[i]=0; }
+    for(int i=0;i<MN;i++){ 
+        a[i]=(real_t)(i+2); 
+        b[i]=(real_t)(i-1); 
+        c[i]=0; 
+    }
     #pragma acc data copyin(a[0:MN],b[0:MN]) copyout(c[0:MN])
     {
         #pragma acc parallel loop collapse(ICE_MACRO2)
@@ -48,7 +67,9 @@ int test1(void){
         }
     }
     for (int i = 0; i < MN; ++i) {
-        if (fabs(c[i] - (a[i] + b[i])) > PRECISION) err++;
+        if (fabs(c[i] - (a[i] + b[i])) > PRECISION){
+            err++;
+        }
     }
 
     free(a);
@@ -58,8 +79,6 @@ int test1(void){
 }
 #endif
 #ifndef T2
-//T2:syntax,tile-clause,runtime,loop,V:3.4-
-// tile(ICE_TILE1) macro ICE
 int test2(void){
     int err = 0;
     const int M = 256;
@@ -70,7 +89,11 @@ int test2(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<M;i++){ a[i]=(real_t)i; c[i]=0; }
+    for(int i=0;i<M;i++){
+        a[i]=(real_t)i; 
+        c[i]=0; 
+    }
+    
     #pragma acc data copyin(a[0:M]) copyout(c[0:M])
     {
         #pragma acc parallel loop tile(ICE_TILE1)
@@ -79,7 +102,9 @@ int test2(void){
         }
     }
     for(int i=0;i<M;i++){
-        if (fabs(c[i] - a[i]*(real_t)2.0) > PRECISION) err++;
+        if (fabs(c[i] - a[i]*(real_t)2.0) > PRECISION){
+            err++;
+        }
     }
     free(a);
     free(c);
@@ -87,8 +112,6 @@ int test2(void){
 }
 #endif
 #ifndef T3
-//T3:syntax,tile-clause,runtime,loop,V:3.4-
-// tile(ICE_ENUM2, ICE_MACRO2) enum + macro ICE
 int test3(void){
     int err = 0;
     const int M = 64, N = 40;
@@ -102,7 +125,13 @@ int test3(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<MN;i++){ a[i]=(real_t)(i+1); b[i]=(real_t)(3*i); c[i]=0; }
+    
+    for(int i=0;i<MN;i++){
+        a[i]=(real_t)(i+1); 
+        b[i]=(real_t)(3*i); 
+        c[i]=0; 
+    }
+    
     #pragma acc data copyin(a[0:MN],b[0:MN]) copyout(c[0:MN])
     {
         #pragma acc parallel loop tile(ICE_ENUM2, ICE_MACRO2)
@@ -114,7 +143,9 @@ int test3(void){
         }
     }
     for (int i = 0; i < MN; ++i) {
-        if (fabs(c[i] - (a[i] + b[i])) > PRECISION) err++;
+        if (fabs(c[i] - (a[i] + b[i])) > PRECISION){
+            err++;
+        }
     }
 
     free(a);
@@ -124,8 +155,6 @@ int test3(void){
 }
 #endif
 #ifndef T4
-//T4:syntax,cache-directive,runtime,loop,V:3.4-
-// cache: element index uses ICE (enum)
 int test4(void){
     int err = 0;
     const int M = 512;
@@ -136,7 +165,12 @@ int test4(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<M;i++){ a[i]=(real_t)i; c[i]=0; }
+    
+    for(int i=0;i<M;i++){
+        a[i]=(real_t)i;
+        c[i]=0; 
+    }
+    
     #pragma acc data copyin(a[0:M]) copyout(c[0:M])
     {
         #pragma acc parallel loop
@@ -145,17 +179,19 @@ int test4(void){
             c[i] = a[i] + (real_t)1.0;
         }
     }
+    
     for(int i=0;i<M;i++){
-        if (fabs(c[i] - (a[i]+(real_t)1.0)) > PRECISION) err++;
+        if (fabs(c[i] - (a[i]+(real_t)1.0)) > PRECISION){
+            err++;
+        }
     }
+    
     free(a);
     free(c);
     return err;
 }
 #endif
 #ifndef T5
-//T5:syntax,cache-directive,runtime,loop,V:3.4-
-// cache: subarray lower:length uses ICE lower + ICE length
 int test5(void){
     int err = 0;
     const int M = 512;
@@ -166,7 +202,10 @@ int test5(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<M;i++){ a[i]=(real_t)i; c[i]=a[i]; }
+    for(int i=0;i<M;i++){
+        a[i]=(real_t)i; 
+        c[i]=a[i]; 
+    }
     #pragma acc data copyin(a[0:M]) copy(c[0:M])
     {
         #pragma acc parallel loop
@@ -175,18 +214,19 @@ int test5(void){
             c[i] = c[i] + (real_t)2.0;
         }
     }
+    
     for(int i=0;i<M;i++){
-        if (fabs(c[i] - (a[i]+(real_t)2.0)) > PRECISION) err++;
+        if (fabs(c[i] - (a[i]+(real_t)2.0)) > PRECISION){
+            err++;
+        }
     }
+    
     free(a);
     free(c);
     return err;
 }
 #endif
 #ifndef T6
-//T6:syntax,gang-clause,runtime,loop,V:3.4-
-// gang(dim:ICE_ENUM2) where dim is an integral-constant-expression (enum), must evaluate to 1..3
-// NOTE: Some compilers may not support the 'dim:' keyword form yet; keep this as spec conformance coverage.
 int test6(void){
     int err = 0;
     const int M = 512;
@@ -197,7 +237,12 @@ int test6(void){
         free(c);
         return 1;
     }
-    for(int i=0;i<M;i++){ a[i]=(real_t)i; c[i]=0; }
+    
+    for(int i=0;i<M;i++){
+        a[i]=(real_t)i;
+        c[i]=0; 
+    }
+    
     #pragma acc data copyin(a[0:M]) copyout(c[0:M])
     {
         #pragma acc parallel loop gang(dim:ICE_ENUM2)
@@ -205,9 +250,13 @@ int test6(void){
             c[i] = a[i] * (real_t)2.0;
         }
     }
+    
     for(int i=0;i<M;i++){
-        if (fabs(c[i] - a[i]*(real_t)2.0) > PRECISION) err++;
+        if (fabs(c[i] - a[i]*(real_t)2.0) > PRECISION){
+            err++;
+        }
     }
+    
     free(a);
     free(c);
     return err;
@@ -216,22 +265,58 @@ int test6(void){
 int main(void){
     int failcode=0, failed;
 #ifndef T1
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test1(); if(failed) failcode|=(1<<0);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test1();
+    }
+    if(failed){
+        failcode|=(1<<0);
+    }
 #endif
 #ifndef T2
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test2(); if(failed) failcode|=(1<<1);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test2(); 
+    }
+    if(failed){
+        failcode|=(1<<1);
+    }
 #endif
 #ifndef T3
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test3(); if(failed) failcode|=(1<<2);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test3(); 
+    }
+    if(failed){
+        failcode|=(1<<2);
+    }
 #endif
 #ifndef T4
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test4(); if(failed) failcode|=(1<<3);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test4(); 
+    }
+    if(failed){
+        failcode|=(1<<3);
+    }
 #endif
 #ifndef T5
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test5(); if(failed) failcode|=(1<<4);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test5(); 
+    }
+    if(failed){
+        failcode|=(1<<4);
+    }
 #endif
 #ifndef T6
-    failed=0; for(int i=0;i<NUM_TEST_CALLS;i++) failed+=test6(); if(failed) failcode|=(1<<5);
+    failed=0; 
+    for(int i=0;i<NUM_TEST_CALLS;i++){
+        failed+=test6(); 
+    }
+    if(failed){
+        failcode|=(1<<5);
+    }
 #endif
     return failcode;
 }
