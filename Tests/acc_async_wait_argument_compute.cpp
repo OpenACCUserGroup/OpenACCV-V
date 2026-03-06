33aab754dd32c252acb4354bec600873abc4dc3e
Ryanpadrone <159075564+Ryanpadrone@users.noreply.github.com>
2026-03-06 18:23:29 -0500

    Update acc_async_wait_argument_compute.cpp

--- Tests/acc_async_wait_argument_compute.cpp
+++ Tests/acc_async_wait_argument_compute.cpp
@@ -27,7 +27,9 @@ int test1() {
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c) return 1;
+    if (!a || !b || !c){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -41,16 +43,22 @@ int test1() {
     #pragma acc data copyin(a[0:n], b[0:n]) copy(c[0:n])
     {
         #pragma acc parallel loop present(a[0:n], b[0:n], c[0:n]) async(q0)
-        for (int i = 0; i < n; ++i) c[i] = a[i] + b[i];
+        for (int i = 0; i < n; ++i){
+            c[i] = a[i] + b[i];
+        }
 
         #pragma acc wait
     }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(c[i] - (a[i] + b[i])) > PRECISION) err++;
+        if (std::fabs(c[i] - (a[i] + b[i])) > PRECISION){
+            err++;
+        }
     }
 
-    std::free(a); std::free(b); std::free(c);
+    std::free(a); 
+    std::free(b);
+    std::free(c);
     return err;
 }
 #endif
@@ -65,7 +73,9 @@ int test2() {
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *d = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *e = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c || !d || !e) return 1;
+    if (!a || !b || !c || !d || !e){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -76,24 +86,36 @@ int test2() {
     #pragma acc enter data copyin(a[0:n], b[0:n]) create(c[0:n], d[0:n], e[0:n])
 
     #pragma acc parallel loop present(a[0:n], c[0:n]) async(1)
-    for (int i = 0; i < n; ++i) c[i] = a[i] * (real_t)2;
+    for (int i = 0; i < n; ++i){
+        c[i] = a[i] * (real_t)2;
+    }
 
     #pragma acc parallel loop present(b[0:n], d[0:n]) async(2)
-    for (int i = 0; i < n; ++i) d[i] = b[i] * (real_t)3;
+    for (int i = 0; i < n; ++i){
+        d[i] = b[i] * (real_t)3;
+    }
 
     #pragma acc parallel loop present(c[0:n], d[0:n], e[0:n]) async(3) wait(queues: 1, 2)
-    for (int i = 0; i < n; ++i) e[i] = c[i] + d[i];
+    for (int i = 0; i < n; ++i){
+        e[i] = c[i] + d[i];
+    }
 
     #pragma acc update self(e[0:n]) async(3)
     while (!acc_async_test(3)) { }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(e[i] - (a[i]*(real_t)2 + b[i]*(real_t)3)) > PRECISION) err++;
+        if (std::fabs(e[i] - (a[i]*(real_t)2 + b[i]*(real_t)3)) > PRECISION){
+            err++;
+        }
     }
 
     #pragma acc exit data delete(a[0:n], b[0:n], c[0:n], d[0:n], e[0:n])
 
-    std::free(a); std::free(b); std::free(c); std::free(d); std::free(e);
+    std::free(a); 
+    std::free(b); 
+    std::free(c); 
+    std::free(d); 
+    std::free(e);
     return err;
 }
 #endif
@@ -109,7 +131,9 @@ int test3() {
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c) return 1;
+    if (!a || !b || !c){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -120,20 +144,28 @@ int test3() {
     #pragma acc enter data copyin(a[0:n], b[0:n]) create(c[0:n])
 
     #pragma acc parallel loop present(a[0:n], b[0:n], c[0:n]) async
-    for (int i = 0; i < n; ++i) c[i] = a[i] - b[i];
+    for (int i = 0; i < n; ++i){
+        c[i] = a[i] - b[i];
+    }
 
-    if (acc_get_default_async() != q) err++;
+    if (acc_get_default_async() != q){
+        err++;
+    }
 
     #pragma acc update self(c[0:n]) async(q)
     while (!acc_async_test(q)) { }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(c[i] - (a[i] - b[i])) > PRECISION) err++;
+        if (std::fabs(c[i] - (a[i] - b[i])) > PRECISION){
+            err++;
+        }
     }
 
     #pragma acc exit data delete(a[0:n], b[0:n], c[0:n])
 
-    std::free(a); std::free(b); std::free(c);
+    std::free(a);
+    std::free(b); 
+    std::free(c);
     return err;
 }
 #endif
@@ -145,27 +177,39 @@ int test4() {
 
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b) return 1;
+    if (!a || !b){
+        return 1;
+    }
 
-    for (int i = 0; i < n; ++i) { a[i] = (real_t)1; b[i] = (real_t)0; }
+    for (int i = 0; i < n; ++i){
+        a[i] = (real_t)1; 
+        b[i] = (real_t)0; 
+    }
 
     #pragma acc data copy(a[0:n], b[0:n])
     {
         #pragma acc parallel loop async(1)
-        for (int i = 0; i < n; ++i) b[i] = a[i] * (real_t)2;
+        for (int i = 0; i < n; ++i){
+            b[i] = a[i] * (real_t)2;
+        }
 
         #pragma acc parallel loop async(2) wait(devnum: 0 : 1)
-        for (int i = 0; i < n; ++i) a[i] = b[i] + (real_t)1;
+        for (int i = 0; i < n; ++i){
+            a[i] = b[i] + (real_t)1;
+        }
 
         #pragma acc update self(a[0:n]) async(2)
         while (!acc_async_test(2)) { }
     }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(a[i] - (real_t)3) > PRECISION) err++;
+        if (std::fabs(a[i] - (real_t)3) > PRECISION){
+            err++;
+        }
     }
 
-    std::free(a); std::free(b);
+    std::free(a); 
+    std::free(b);
     return err;
 }
 #endif
@@ -178,7 +222,9 @@ int test5() {
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c) return 1;
+    if (!a || !b || !c){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -189,20 +235,28 @@ int test5() {
     #pragma acc data copyin(a[0:n], b[0:n]) copy(c[0:n])
     {
         #pragma acc parallel loop async(1)
-        for (int i = 0; i < n; ++i) c[i] = a[i] + b[i];
+        for (int i = 0; i < n; ++i){
+            c[i] = a[i] + b[i];
+        }
 
         #pragma acc parallel loop async(2) wait(devnum: 0 : queues: 1)
-        for (int i = 0; i < n; ++i) c[i] = c[i] * (real_t)2;
+        for (int i = 0; i < n; ++i){
+            c[i] = c[i] * (real_t)2;
+        }
 
         #pragma acc update self(c[0:n]) async(2)
         while (!acc_async_test(2)) { }
     }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(c[i] - (real_t)2*(a[i] + b[i])) > PRECISION) err++;
+        if (std::fabs(c[i] - (real_t)2*(a[i] + b[i])) > PRECISION){
+            err++;
+        }
     }
 
-    std::free(a); std::free(b); std::free(c);
+    std::free(a); 
+    std::free(b); 
+    std::free(c);
     return err;
 }
 #endif
@@ -215,7 +269,9 @@ int test6() {
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c) return 1;
+    if (!a || !b || !c){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -226,16 +282,22 @@ int test6() {
     #pragma acc data copyin(a[0:n], b[0:n]) copy(c[0:n])
     {
         #pragma acc parallel loop async(acc_async_sync)
-        for (int i = 0; i < n; ++i) c[i] = a[i] + b[i];
+        for (int i = 0; i < n; ++i){
+            c[i] = a[i] + b[i];
+        }
 
         #pragma acc wait
     }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(c[i] - (a[i] + b[i])) > PRECISION) err++;
+        if (std::fabs(c[i] - (a[i] + b[i])) > PRECISION){
+            err++;
+        }
     }
 
-    std::free(a); std::free(b); std::free(c);
+    std::free(a); 
+    std::free(b); 
+    std::free(c);
     return err;
 }
 #endif
@@ -248,7 +310,9 @@ int test7() {
     real_t *a = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *b = (real_t*)std::malloc(n * sizeof(real_t));
     real_t *c = (real_t*)std::malloc(n * sizeof(real_t));
-    if (!a || !b || !c) return 1;
+    if (!a || !b || !c){
+        return 1;
+    }
 
     for (int i = 0; i < n; ++i) {
         a[i] = std::rand() / (real_t)(RAND_MAX / 10);
@@ -259,16 +323,22 @@ int test7() {
     #pragma acc data copyin(a[0:n], b[0:n]) copy(c[0:n])
     {
         #pragma acc parallel loop async(acc_async_noval)
-        for (int i = 0; i < n; ++i) c[i] = a[i] - b[i];
+        for (int i = 0; i < n; ++i){
+            c[i] = a[i] - b[i];
+        }
 
         #pragma acc wait
     }
 
     for (int i = 0; i < n; ++i) {
-        if (std::fabs(c[i] - (a[i] - b[i])) > PRECISION) err++;
+        if (std::fabs(c[i] - (a[i] - b[i])) > PRECISION){
+            err++;
+        }
     }
 
-    std::free(a); std::free(b); std::free(c);
+    std::free(a); 
+    std::free(b); 
+    std::free(c);
     return err;
 }
 #endif
@@ -279,38 +349,66 @@ int main() {
 
 #ifndef T1
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test1();
-    if (failed) failcode |= (1 << 0);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test1();
+    }
+    if (failed){
+        failcode |= (1 << 0);
+    }
 #endif
 #ifndef T2
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test2();
-    if (failed) failcode |= (1 << 1);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test2();
+    }
+    if (failed){
+        failcode |= (1 << 1);
+    }
 #endif
 #ifndef T3
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test3();
-    if (failed) failcode |= (1 << 2);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test3();
+    }
+    if (failed){
+        failcode |= (1 << 2);
+    }
 #endif
 #ifndef T4
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test4();
-    if (failed) failcode |= (1 << 3);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test4();
+    }
+    if (failed){
+        failcode |= (1 << 3);
+    }
 #endif
 #ifndef T5
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test5();
-    if (failed) failcode |= (1 << 4);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test5();
+    }
+    if (failed){
+        failcode |= (1 << 4);
+    }
 #endif
 #ifndef T6
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test6();
-    if (failed) failcode |= (1 << 5);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test6();
+    }
+    if (failed){
+        failcode |= (1 << 5);
+    }
 #endif
 #ifndef T7
     failed = 0;
-    for (int i = 0; i < NUM_TEST_CALLS; ++i) failed += test7();
-    if (failed) failcode |= (1 << 6);
+    for (int i = 0; i < NUM_TEST_CALLS; ++i){
+        failed += test7();
+    }
+    if (failed){
+        failcode |= (1 << 6);
+    }
 #endif
 
     return failcode;
